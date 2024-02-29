f93812745d435b524f5829afd9c712b2ffa84655
driven <aldazpaulo@gmail.com>
2024-02-28 22:52:16 -0500

    un navbar de ejemplo

---
+++ src/components/NavBar.vue
@@ -0,0 +1,39 @@
+<template>
+    <nav class="navbar navbar-expand-lg navbar-light" style="background-color: #e3f2fd;">
+    <div class="container-fluid">
+      <router-link class="navbar-brand" to="#">Navbar</router-link>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarColor03" aria-controls="navbarColor03" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarColor03">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+            <li class="nav-item dropdown">
+          <router-link class="nav-link dropdown-toggle" to="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Dropdown
+          </router-link>
+          <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+            <li><router-link class="dropdown-item" to="#">Action</router-link></li>
+            <li><router-link class="dropdown-item" to="#">Another action</router-link></li>
+            <li><hr class="dropdown-divider"></li>
+            <li><router-link class="dropdown-item" to="#">Something else here</router-link></li>
+          </ul>
+        </li>
+        </ul>
+        <form class="d-flex">
+          <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+          <button class="btn btn-outline-primary" type="submit">Search</button>
+        </form>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/router/router.js
@@ -0,0 +1,14 @@
+import { createRouter, createWebHashHistory } from "vue-router";
+
+const routes = [
+    // {
+    //     path: '/actualizar',
+    //     component: EstudiantesActualizar
+    // },
+    
+]
+
+
+const router = createRouter({history: createWebHashHistory(), routes})
+
+export default router
