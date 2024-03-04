bf37e30f7cf28631ca1a5bd6a04a57af7d339f7d
driven <aldazpaulo@gmail.com>
2024-03-04 18:55:57 -0500

    validacion

--- src/components/RegistroClienteEmpleado.vue
+++ src/components/RegistroClienteEmpleado.vue
@@ -1,68 +1,70 @@
 <template>
-  <div class="contenedor">
-    <div class="titulo">
-        <h2>Ingresar un Cliente.</h2>
-    </div>
-    <form class="formulario" @submit.prevent="registrarCliente">
-        <div class="mb-3">
-            <label for="nombre" class="form-label" required>Nombre</label>
-            <input v-model="cliente.nombre" type="text" class="form-control" id="nombre" required>
+    <div class="contenedor">
+        <div class="titulo">
+            <h2>Ingresar un Cliente.</h2>
         </div>
-        <div class="mb-3">
-            <label for="apellido" class="form-label" required>Apellido</label>
-            <input v-model="cliente.apellido" type="text" class="form-control" id="apellido" required>
-        </div>
-        <div class="mb-3">
-            <label for="cedula" class="form-label" required>Cedula</label>
-            <input v-model="cliente.cedula" type="text" class="form-control" id="cedula" required>
-        </div>
-        <div class="mb-3">
-            <label for="fechaNacimiento" class="form-label">Fecha de nacimiento</label>
-            <input v-model="cliente.fechaNacimiento" type="date" class="form-control" id="fechaNacimiento">
-        </div>
-        <div class="mb-3">
-            <label for="genero" class="form-label">Genero</label>
-                <select v-model="cliente.genero" id="genero" class="form-select" >
+        <form class="formulario" @submit.prevent="registrarCliente">
+            <div class="mb-3">
+                <label for="nombre" class="form-label" required>Nombre</label>
+                <input v-model="cliente.nombre" type="text" class="form-control" id="nombre" required>
+            </div>
+            <div class="mb-3">
+                <label for="apellido" class="form-label" required>Apellido</label>
+                <input v-model="cliente.apellido" type="text" class="form-control" id="apellido" required>
+            </div>
+            <div class="mb-3">
+                <label for="cedula" class="form-label" required>Cedula</label>
+                <input v-model="cliente.cedula" type="text" class="form-control" id="cedula" required>
+            </div>
+            <div class="mb-3">
+                <label for="fechaNacimiento" class="form-label">Fecha de nacimiento</label>
+                <input v-model="cliente.fechaNacimiento" type="date" class="form-control" id="fechaNacimiento">
+            </div>
+            <div class="mb-3">
+                <label for="genero" class="form-label">Genero</label>
+                <select v-model="cliente.genero" id="genero" class="form-select">
                     <option value="masculino">Masculino</option>
                     <option value="femenino">Femenino</option>
                     <option value="otro">Otro</option>
                 </select>
-        </div>
+            </div>
 
 
-        <button type="submit" class="btn btn-primary">Enviar</button>
-    </form>
-  </div>
+            <button type="submit" class="btn btn-primary">Enviar</button>
+        </form>
+    </div>
 </template>
 
 <script>
 import { insertarFachada } from "@/helpers/clienteCliente";
 export default {
-     data() {
+    data() {
         return {
             cliente: {
-                cedula: null, 
+                cedula: null,
                 nombre: null,
                 apellido: null,
                 fechaNacimiento: null,
                 genero: null,
-                tipoRegistro:"E"
+                tipoRegistro: "E"
             }
         };
     },
     methods: {
         async registrarCliente() {
-            this.cliente.fechaNacimiento = new Date(this.cliente.fechaNacimiento);
-                var msj = await insertarFachada(this.cliente); 
-                alert(msj);
+            try {
+                this.cliente.fechaNacimiento = new Date(this.cliente.fechaNacimiento);
+                const response = await insertarFachada(this.cliente);
+                console.log(response);
+                alert(response)
+            } catch (error) {
+                alert("La cédula ingresada ya se encuentra ingresada en la sistema");
+            }
         }
     }
-    
+
 
 };
 </script>
 
-<style scoped>
-
-
-</style>
+<style scoped></style>
--- src/helpers/clienteCliente.js
+++ src/helpers/clienteCliente.js
@@ -1,109 +1,118 @@
 import axios from "axios";
 
-
 const insertarCliente = async (body) => {
-    return axios.post("http://localhost:8080/api/v1.0/Renta/clientes", body).then(r => r.data);
-}
+  
+    const response = axios
+      .post("http://localhost:8080/api/v1.0/Renta/clientes", body)
+      .then((r) => r.data);
+    return response;
+  
+};
 
-const buscarCedula = async (cedula) =>{
-    //http://localhost:8080/api/v1.0/Renta/clientes/123
-    return axios.get("http://localhost:8080/api/v1.0/Renta/clientes/"+cedula).then(r => r.data);
-}
+const buscarCedula = async (cedula) => {
+  //http://localhost:8080/api/v1.0/Renta/clientes/123
+  return axios
+    .get("http://localhost:8080/api/v1.0/Renta/clientes/" + cedula)
+    .then((r) => r.data);
+};
 const actualizarCliente = async (body) => {
-    return axios.put("http://localhost:8080/api/v1.0/Renta/clientes", body).then(r => r.data);
-
-}
+  return axios
+    .put("http://localhost:8080/api/v1.0/Renta/clientes", body)
+    .then((r) => r.data);
+};
 
 export const insertarFachada = async (body) => {
-
-    return await insertarCliente(body);
-}
-export const buscarFachada = async (cedula) =>{
-    return await buscarCedula(cedula);
-}
+  return await insertarCliente(body);
+};
+export const buscarFachada = async (cedula) => {
+  return await buscarCedula(cedula);
+};
 
 export const actualizarFachadaCliente = async (body) => {
-    return await actualizarCliente(body);
-}
-
+  return await actualizarCliente(body);
+};
 
 export const verClientes = async () => {
-    
-        const response = await axios.get("http://localhost:8080/api/v1.0/Renta/clientes");
-        return response.data;
-    
+  const response = await axios.get(
+    "http://localhost:8080/api/v1.0/Renta/clientes"
+  );
+  return response.data;
 };
 
 export const verClientesFachada = async () => {
-    
-        const clientes = await verClientes();
-        return clientes;
-
+  const clientes = await verClientes();
+  return clientes;
 };
 
 export const eliminarCliente = async (id) => {
-        await axios.delete(`http://localhost:8080/api/v1.0/Renta/clientes/${id}`);
-        console.log(`Cliente con ID ${id} eliminado correctamente`);
-    
+  await axios.delete(`http://localhost:8080/api/v1.0/Renta/clientes/${id}`);
+  console.log(`Cliente con ID ${id} eliminado correctamente`);
 };
 
 export const eliminarClienteFachada = async (id) => {
-    
-        await eliminarCliente(id);
-        console.log(`Cliente con ID ${id} eliminado correctamente a través de la fachada`);
-    
+  await eliminarCliente(id);
+  console.log(
+    `Cliente con ID ${id} eliminado correctamente a través de la fachada`
+  );
 };
 
 const actualizarClienteId = async (cedula, body) => {
-    console.log(body);
-    const data = await axios.put(`http://localhost:8080/api/v1.0/Renta/clientes/${cedula}`, body).then(r => r.data);
-    console.log(data)
+  console.log(body);
+  const data = await axios
+    .put(`http://localhost:8080/api/v1.0/Renta/clientes/${cedula}`, body)
+    .then((r) => r.data);
+  console.log(data);
+};
 
-}
-
-
-export const  actualizarFachadaClienteId = async (cedula, body) => {
-    
-    await actualizarClienteId(cedula, body);
-    
-}
+export const actualizarFachadaClienteId = async (cedula, body) => {
+  await actualizarClienteId(cedula, body);
+};
 
 const reservarVehiculoLogic = async (reserva) => {
-    try {
-        const response = await axios.post("http://localhost:8080/api/v1.0/Renta/vehiculos/reserva", reserva);
-        return response.data;
-    } catch (error) {
-        console.error("Error al reservar vehículo:", error);
-        throw error; 
-    }
+  try {
+    const response = await axios.post(
+      "http://localhost:8080/api/v1.0/Renta/vehiculos/reserva",
+      reserva
+    );
+    return response.data;
+  } catch (error) {
+    console.error("Error al reservar vehículo:", error);
+    throw error;
+  }
 };
 
 export const reservarVehiculoFachada = async (reserva) => {
-    try {
-        return await reservarVehiculoLogic(reserva);
-    } catch (error) {
-        console.error("Error al reservar vehículo:", error);
-        throw error; 
-    }
+  try {
+    return await reservarVehiculoLogic(reserva);
+  } catch (error) {
+    console.error("Error al reservar vehículo:", error);
+    throw error;
+  }
 };
 
-export const busquedaVehiculoFecha = async (placa,fechaInicio,fechaFin) => {
-    //return await obtenerEmpleadoAPI(id);
-    return await busquedaVehiculoFechaAxios(placa,fechaInicio,fechaFin);
-}
-const busquedaVehiculoFechaAxios=async(placa,fechaInicio,fechaFin)=>{
-    const data = axios.get(`http://localhost:8080/api/v1.0/Renta/vehiculos/busqueda?placa=${placa}&fechaInicio=${fechaInicio}&fechaFin=${fechaFin}`).then(r => r.data)
-    console.log(data)
-    return data
-}
+export const busquedaVehiculoFecha = async (placa, fechaInicio, fechaFin) => {
+  //return await obtenerEmpleadoAPI(id);
+  return await busquedaVehiculoFechaAxios(placa, fechaInicio, fechaFin);
+};
+const busquedaVehiculoFechaAxios = async (placa, fechaInicio, fechaFin) => {
+  const data = axios
+    .get(
+      `http://localhost:8080/api/v1.0/Renta/vehiculos/busqueda?placa=${placa}&fechaInicio=${fechaInicio}&fechaFin=${fechaFin}`
+    )
+    .then((r) => r.data);
+  console.log(data);
+  return data;
+};
 
 export const reservarVehiculo = async (reser) => {
-    //return await obtenerEmpleadoAPI(id);
-    return await reservarVehiculoAxios(reser);
-}
+  //return await obtenerEmpleadoAPI(id);
+  return await reservarVehiculoAxios(reser);
+};
 
-const reservarVehiculoAxios=async(reser)=>{
-    const data = axios.post(`http://localhost:8080/api/v1.0/Renta/vehiculos/reserva`,reser).then(r => r.data)
-    console.log(data)
-    return data
-}
+const reservarVehiculoAxios = async (reser) => {
+  const data = axios
+    .post(`http://localhost:8080/api/v1.0/Renta/vehiculos/reserva`, reser)
+    .then((r) => r.data);
+  console.log(data);
+  return data;
+};
